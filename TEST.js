953ff29e06e10420477d61c450eb477f95ea3ba2
Nehaa-verma <145045421+Nehaa-verma@users.noreply.github.com>
2024-11-07 13:13:44 +0530

    Add files via upload

---
+++ TEST.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: 2
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: 1
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        options: ["Atlantic", "Indian", "Arctic", "Pacific"],
+        answer: 3
+    },
+    {
+        question: "What is the capital of India?",
+        options: ["New Delhi","Lucknow","Patna","Jaipur"]
+    }
+];
